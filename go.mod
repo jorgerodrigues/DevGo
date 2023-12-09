02019659ecf496b0c7045e6c3272d2aa383b6210
Jorge Lopes Rodrigues <jorgelopes@hey.com>
2023-12-09 13:44:44 +0100

    First commit - uuid

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/jorgerodrigues/devgo
+
+go 1.21.0
+
+require (
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/atotto/clipboard"
+	"github.com/google/uuid"
+)
+
+func main() {
+	u, err := uuid.NewRandom()
+	if err != nil {
+		fmt.Printf("Something went wrong: %s", err)
+		return
+	}
+	uuidString := u.String()
+
+	// Copy to clipboard
+	err = clipboard.WriteAll(uuidString)
+	if err != nil {
+		fmt.Printf("Failed to copy to clipboard: %s", err)
+	} else {
+		fmt.Println("UUID copied to clipboard:", uuidString)
+	}
+}
